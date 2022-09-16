2df17815df9f08fffb72008939016fb306e92080
JADEN <29948746+JADENgygo@users.noreply.github.com>
2022-09-16 21:19:31 +0900

    fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ const Home: NextPage = () => {
     <div className="container">
       <div className="mt-3">プリコネRのツール・ゲーム集だよ</div>
       <div className="alert alert-info mt-3" role="alert">
-        9.14 <a href="https://priconne-yabai.vercel.app">プリコネヤバイ</a> リリース
+        2022.9.14 <a href="https://priconne-yabai.vercel.app">プリコネヤバイ</a> リリース
       </div>
       <div className="row text-center gy-3">
         <hr />
